3fab1473a3f214c7206f0f8ecb2a7ab9bd533ba0
MG <m2biel000@icloud.com>
2024-09-14 10:35:22 -0300

    created db, add products route done

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 #import
-from flask import Flask
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///ecommerce.db'
 
-#Define main route
+db = SQLAlchemy(app)
+
+#Modeling
+#Product (id, name, price, description)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+
+@app.route('/api/products/add', methods=["POST"])
+def add_product():
+    data = request.json
+    product = Product(name=data['name'],price=data['price'],description=data.get('description',''))
+    db.session.add(product)
+    db.session.commit()
+    return "Produto cadastrado com sucesso"
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
