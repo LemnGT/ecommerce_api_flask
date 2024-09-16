7d235f4c0a17af72ccda73d533ab4255e59d728f
MG <m2biel000@icloud.com>
2024-09-16 12:10:54 -0300

    update products by id route done

--- app.py
+++ app.py
@@ -56,6 +56,27 @@ def get_product_details(product_id):
     #if invalid
     return jsonify({"message":"Product not found"}),404
 
+@app.route('/api/products/update/<int:product_id>', methods=["PUT"])
+def update_product(product_id):
+
+    #get product from db
+    product=Product.query.get(product_id)
+    #if invalid
+    if not product:
+        return jsonify({"message":"Product not found"}),404
+    
+    data = request.json
+    if 'name' in data:
+        product.name=data['name']
+        
+    if 'price' in data:
+        product.price=data['price']
+        
+    if 'description' in data:
+        product.description=data['description']
+
+    db.session.commit()
+    return jsonify({"message":"Product updated successfully"})
 @app.route('/')
 def hello_world():
     return 'Hello World!'
