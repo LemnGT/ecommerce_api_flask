deee779762610c477e22cc05c58be908903afbee
MG <m2biel000@icloud.com>
2024-09-18 12:22:27 -0300

    Add, Delete and Update only when logged

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_login import UserMixin, login_user, LoginManager, login_required
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 
 
 app = Flask(__name__)
@@ -30,6 +30,11 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
 
+#Auth
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route('/login',methods=["POST"])
 def login():
     data=request.json
@@ -39,6 +44,13 @@ def login():
         login_user(user)
         return jsonify({"message":"Logged in successfully"})
     return jsonify({"message":"Unauthorized. Invalid credentials"}),401
+
+@app.route('/logout', methods=["POST"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message":"Logout in successfully"})
+
 
 @app.route('/api/products/add', methods=["POST"])
 @login_required
